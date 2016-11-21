cfd81101e51a8a32c39d5fcff2146d7cbbcb51d4
Taioli Francesco <taioli.francesco98@gmail.com>
2016-11-21 21:28:19 +0100

    add header, install semantic

--- routes/user/users.js
+++ routes/user/users.js
@@ -48,7 +48,6 @@ module.exports = function(app, passport) {
     /**
      * registration page
      */
-
     app.get('/signup', function(req, res) {
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-
+app.use(express.static(__dirname + '/public'));
 
 
 // routes ======================================================================
