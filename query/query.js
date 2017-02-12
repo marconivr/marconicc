c6a4927833541b5cdade70b4c5ca98e0872bcdfa
mattiacorradi <mattia.corradi.tr@gmail.com>
2017-02-12 15:27:30 +0100

    move query class and fix relative path

--- query/query.js
+++ query/query.js
@@ -1,20 +1,24 @@
+
 /**
  * Created by mattiacorradi on 11/02/2017.
  */
 
+
+//ALL THE QUERY MUST BE INSERTED IN THIS CLASS!!!
+
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
-var dbconfig = require('./database');
+var dbconfig = require('./../config/database');
 var connection = mysql.createConnection(dbconfig.connection);
 
 connection.query('USE ' + dbconfig.database);
 
 
-
 module.exports = {
 
-    getUsersInformations:function getUsersInformations(){
 
+    //test method
+    getUsersInformations:function getUsersInformations(){
         connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
             done(err, rows[0]);
         });
