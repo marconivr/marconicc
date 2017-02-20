25d98a3e0b4146f76d4711809e550270f0fc10db
Alberto Strina <alberto.strina98@gmail.com>
2017-02-20 15:22:50 +0100

    aggiunto altri element su examples per testare

--- routes/main.js
+++ routes/main.js
@@ -52,7 +52,7 @@ module.exports = function (app, passport) {
      */
     app.get('/signup', function (req, res) {
         // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', {message: req.flash('signupMessage')});
+        res.render('signup.ejs', {message: req.flash('signupMessage'), pageTitle:"signup"});
     });
 
 
