f7d680b337c5d1f8823ee850fb30f3f6138d03b9
michele <dallachiaram@gmail.com>
2017-02-28 18:07:16 +0100

    algorithm.js

--- routes/algorithm.js
+++ routes/algorithm.js
@@ -1,5 +1,5 @@
 /**
- * Created by michele on 2/13/17..
+ * Created by michele on 2/13/17.
  */
 
 var middleware = require('./middleware/middleware');
@@ -12,7 +12,9 @@ var settings = {
     min_al: 25,
     max_fem: 3,
     max_str: 5,
+    iniziale:3,
     stessa_pr: 4,
+    nazionalita:4,
     media_min: 7.3,
     media_max: 8.0,
     boc: 2,
@@ -20,10 +22,16 @@ var settings = {
 }
 var priority = {}
 var listaAlunni = [];
-var listaClassi = [];  //esempio [{nome:"1AI", proprieta:{alunni:23, femmine:2}, alunni:[{nome:"Mario", cognome:"Rossi"}]}]
+var listaClassi = []; //esempio [{nome:"1AI", proprieta:{alunni:23, femmine:2}, alunni:[{nome:"Mario", cognome:"Rossi"}]}]
 
 module.exports = {
-    loadListAlunni: function (classe, callback) {
+
+    /**
+     * main dell'algoritmo
+     * @param classe
+     * @param callback
+     */
+    main: function (classe, callback) {
         if (classe.toLowerCase() == "prima") {
             query.getStudentiPrima(function (err, results) {
                 if (err)
@@ -44,12 +52,12 @@ module.exports = {
                                 callback();
                             },
                             function (callback) {
-                                module.exports.numberOfClassi("prima", function () {
+                                module.exports.generaListaClassi("prima", function () {
                                     callback();
                                 });
                             },
                             function (callback) {
-                                module.exports.createListClassi("prima", function () {
+                                module.exports.popolaListaClassiRandom("prima", function () {
                                     callback();
                                 });
                             }
@@ -67,7 +75,13 @@ module.exports = {
         }
     },
 
-    numberOfClassi: function (classe, callback) {
+    /**
+     * generaListaClassi è una funzione che genera oggetti classe {nome: classe, proprieta:{}, alunni: []}
+     * in base agli alunni minimi
+     * @param classe
+     * @param callback
+     */
+    generaListaClassi: function (classe, callback) {
         if (classe.toLowerCase() == "prima") {
             var num = Math.round(listaAlunni.length / (settings.min_al));
             for (i = 0; i < num; i++) {
@@ -79,7 +93,12 @@ module.exports = {
         callback();
     },
 
-    createListClassi: function (classe, callback) {
+    /**
+     * popolaListaClassiRandom crea una le classi in maniera random
+     * @param classe
+     * @param callback
+     */
+    popolaListaClassiRandom: function (classe, callback) {
         if (classe.toLowerCase() == "prima") {
             while (listaAlunni.length != 0) {
                 for (k = 0; k < listaClassi.length; k++) {
@@ -91,13 +110,33 @@ module.exports = {
                             break;
                         }
                     }
+                    listaClassi[k].alunni = module.exports.removeUndefinedDaArray(listaClassi[k].alunni);
+                    listaClassi[k].proprieta = module.exports.createProprietaClasse(listaClassi[k].alunni);
                 }
-
             }
             callback();
         }
     },
 
+    /**
+     * createProprietaClasse crea le proprieta di una lista di alunni
+     * @param listaAlunniClasse
+     * @returns {{alunni: *, femmine: (*|Number), media: *, residenza: (*|Array), bocciati: (*|number), iniziale: *}}
+     */
+    createProprietaClasse: function (listaAlunniClasse) {
+        var nAlunni = module.exports.countAlunni(listaAlunniClasse);
+        var nFemmine = module.exports.countFemmine(listaAlunniClasse);
+        var media = module.exports.mediaClasse(listaAlunniClasse);
+        var residenza = module.exports.countStessaResid(listaAlunniClasse);
+        var bocciati = module.exports.countBocciati(listaAlunniClasse);
+        var iniziale = module.exports.countInizialeCognome(listaAlunniClasse);
+        return {alunni:nAlunni, femmine:nFemmine, media:media, residenza:residenza, bocciati:bocciati, iniziale:iniziale};
+    },
+
+    /**
+     * listaNomiClassi è una funzione che ritorna la lista di nomi delle classi
+     * @returns {Array}
+     */
     listaNomiClassi: function () {
         var listaNomi = [];
         for (var k = 0; k < listaClassi.length; k++) {
@@ -106,6 +145,14 @@ module.exports = {
         return listaNomi;
     },
 
+    //##################################################################################################################
+    /**--------------------------------------FUNZIONI PER COMPORRE CLASSI---------------------------------------------*/
+    //##################################################################################################################
+
+    /**
+     * countAlunni ritorna il numero di alunni per classe
+     * @param listaAlunniClasse
+     */
     countAlunni : function (listaAlunniClasse) {
         return listaAlunniClasse.length;
     },
@@ -117,110 +164,211 @@ module.exports = {
      */
     countFemmine : function (listaAlunniClasse) {
         var cont = 0;
+
         for (var i = 0; i < listaAlunniClasse.length; i++) {
-            if (listaAlunniClasse[i].sesso == "F") {
+            if (listaAlunniClasse[i].sesso.toUpperCase() == "F") {
                 cont++;
             }
         }
         return cont;
     },
 
+    /**
+     * mediaClasse calcola la media di una classe
+     * @param listaAlunniClasse
+     * @returns {number}
+     */
     mediaClasse : function (listaAlunniClasse) {
         var somma = 0;
 
         for (var i = 0; i < listaAlunniClasse.length; i++) {
-            somma += listaAlunniClasse[i].media_voti;
+            somma += Number(listaAlunniClasse[i].media_voti);
         }
-
         return (somma / listaAlunniClasse.length);
 
     },
 
+    /**
+     * countStranieri ritorna una lista di oggetti come nell es. {nazionalita:"brasiliano", num:3}
+     * se superano il valore max impostato in settings
+     * @param listaAlunniClasse
+     * @returns {Array}
+     */
     countStranieri : function (listaAlunniClasse) {
-
-    },
-
-    countBocciati : function (listaAlunniClasse) {
-
-    },
-
-    countStessaProv : function (listaAlunniClasse) {
-        var cont = 0;
-        var cont1 = 0;
-        var prov;
+        var listaNaz = [];
+        var count = 0;
+        var ris = [];
 
         for (var i = 0; i < listaAlunniClasse.length; i++) {
-            prov = classe.alunni[i].cap_provenienza;
+            if (listaAlunniClasse[i].nazionalita.toLowerCase() != "italiana"){
+                listaNaz.push(listaAlunniClasse[i].cap_provenienza);
+            }
+        }
+        listaNaz.sort();
 
-            for (var j = i + 1; j < listaAlunniClasse.length; j++) {
-                if (prov == listaAlunniClasse[j].cap_provenienza){
-                    cont+=1;
+        for (var i = 0; i < listaNaz.length - 1; i++) {
+            if (listaNaz[i] == listaNaz[i + 1]) {
+                count++;
+            } else {
+                if (count > settings.nazionalita){
+                    ris.push({nazionalita:listaNaz[i], num: count + 1});
                 }
+                count = 0;
             }
-            if (cont >= cont1){
-                cont1 = cont;
+        }
+        return ris;
+    },
+
+    /**
+     * countBocciati ritorna il numeero di bocciati per classe
+     * @param listaAlunniClasse
+     * @returns {number}
+     */
+    countBocciati : function (listaAlunniClasse) {
+        var count = 0;
+        var data  = "";
+
+        for (var i = 0; i < listaAlunniClasse.length; i++) {
+            data = listaAlunniClasse[i].data_di_nascita;
+
+            if (typeof data === 'string' || data instanceof String) {
+                data = new Date(Date.parse(data.split(" ")[0]));
             }
-            cont = 0;
+
+            if ((1900 + data.getYear()) < (new Date().getFullYear() - 14)) {
+                count++;
+            }
         }
-        return cont1 + 1;
-    }
 
-}
+        return count;
+    },
 
-/*
-var findPriority = function (classe) {
-
-    for (i = 0; i < priority.length; i++) {
-        switch (priority[i]) {
-            case "alunni":
-                /countAlunni(classe);
-                break;
-            case "femmine":
-                countFemmine(classe);
-                break;
-            case "stranieri":
-                countStranieri(classe);
-                break;
-            case "bocciati":
-                countBocciati(classe);
-                break;
-            case "stessa_provenienza":
-                countStessaProv(classe);
-                break;
-            case "media":
-                mediaClasse(classe);
-                break;
+    /**
+     * countStessaProv ritorna una lista di cap se superano il valore max impostato in settings
+     * @param classe oggetto classe
+     * @returns {Array}
+     */
+    countStessaResid : function (listaAlunniClasse) {
+        var listaCap = [];
+        var count = 0;
+        var ris = [];
+        for (var i = 0; i < listaAlunniClasse.length; i++) {
+            listaCap.push(listaAlunniClasse[i].cap_provenienza);
         }
-    }
-}
-*/
+        listaCap.sort();
 
-/**
- * findClasseFromString data una stringa ritorna l'oggetto classe dato il nome
- * @param nomeClasse stringa
- * @returns {object}
- */
-
-var findClasseFromString = function (nomeClasse) {
-    for (var k = 0; k < listaClassi.length; k++) {
-        if (listaClassi[k].nome == nomeClasse) {
-            return listaClassi[k];
+        for (var i = 0; i < listaCap.length - 1; i++) {
+            if (listaCap[i] == listaCap[i + 1]) {
+                count++;
+            } else {
+                if (count > settings.stessa_pr){
+                    ris.push({cap:listaCap[i], num: count + 1});
+                }
+                count = 0;
+            }
         }
+        return ris;
+    },
+
+    /**
+     * countInizialeCognome ritorna una lista di oggetti {lettera:'a', num: 5}
+     * se superano il valore max impostato in settings
+     * @param listaAlunniClasse
+     * @returns {Array}
+     */
+    countInizialeCognome: function (listaAlunniClasse) {
+        var listaIniz = [];
+        var count = 0;
+        var ris = [];
+        for (var i = 0; i < listaAlunniClasse.length; i++) {
+            listaIniz.push(listaAlunniClasse[i].cognome[0]);
+        }
+        listaIniz.sort();
+
+        for (var i = 0; i < listaIniz.length - 1; i++) {
+            if (listaIniz[i] == listaIniz[i + 1]) {
+                count++;
+            } else {
+                if (count > settings.iniziale){
+                    ris.push({lettera:listaIniz[i], num: count + 1});
+                }
+                count = 0;
+            }
+        }
+        return ris;
+    },
+
+    //##################################################################################################################
+    /**--------------------------------------FINE FUNZIONI PER COMPORRE CLASSI----------------------------------------*/
+    //##################################################################################################################
+
+    //##################################################################################################################
+    /**----------------------------------------------------UTILITY----------------------------------------------------*/
+    //##################################################################################################################
+    /**
+     * findClasseFromString data una stringa ritorna l'oggetto classe dato il nome
+     * @param nomeClasse stringa
+     * @returns {object}
+     */
+    findClasseFromString : function(nomeClasse) {
+        for (var k = 0; k < listaClassi.length; k++) {
+            if (listaClassi[k].nome == nomeClasse) {
+                return listaClassi[k];
+            }
+        }
+        return null;
+    },
+
+    /**
+     * classeIsObj controlla se l'oggetto passato è una string;
+     * se lo è ritorna l'oggetto classe con quel nome, se non lo è ritorna l'oggetto
+     * @param classe
+     * @returns {object}
+     */
+    classeIsObj: function (classe) {
+        if (typeof classe === 'string' || classe instanceof String) {
+            return module.exports.findClasseFromString(classe);
+        }
+
+        else {
+            return classe;
+        }
+    },
+
+    /**
+     * removeUndefinedDaArray rimuove un undefined da un array
+     * @param array
+     */
+    removeUndefinedDaArray: function(array){
+        return array.filter(function(n){ return n != undefined });
     }
-    return null;
+    //##################################################################################################################
+    /**------------------------------------------------FINE UTILITY---------------------------------------------------*/
+    //##################################################################################################################
 }
 
-/**
- * classeIsObj controlla se l'oggetto passato è una string; se lo è ritorna l'oggetto classe con quel nome, se non lo è ritorna l'oggetto
- * @param classe
- * @returns {object}
- */
-var classeIsObj = function (classe) {
-    if (typeof classe === 'string' || classe instanceof String) {
-        return findClasseFromString(classe);
-    }
+/*var findPriority = function (classe) {
 
-    else {
-        return classe;
-    }
-}
+ for (i = 0; i < priority.length; i++) {
+ switch (priority[i]) {
+ case "alunni":
+ countAlunni(classe);
+ break;
+ case "femmine":
+ countFemmine(classe);
+ break;
+ case "stranieri":
+ countStranieri(classe);
+ break;
+ case "bocciati":
+ countBocciati(classe);
+ break;
+ case "stessa_provenienza":
+ countStessaProv(classe);
+ break;
+ case "media":
+ mediaClasse(classe);
+ break;
+ }
+ }
+ }*/
