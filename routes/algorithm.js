30eb0571a9cb8586c5885d3ae0a8113dde2431a7
dallachiaram <dallachiaram@gmail.com>
2017-03-11 12:55:47 +0100

    fix nada de nada colpa del cerry e di nodejs... Grazie Corradi tvb

--- routes/algorithm.js
+++ routes/algorithm.js
@@ -63,14 +63,14 @@ module.exports = {
                                 });
                             }
                             /*
-                            ,
-                            function (callback) {
+                             ,
+                             function (callback) {
 
-                                module.exports.fixClassi(function () {
-                                    callback();
-                                });
-                            }
-                            */
+                             module.exports.fixClassi(function () {
+                             callback();
+                             });
+                             }
+                             */
                         ],
                         function (err, succes) {
                             if (err) {
@@ -195,7 +195,7 @@ module.exports = {
 
                         break;
                     case "femmine":
-                        //module.exports.fixFemmine(listaClassi[k].nome);
+                        module.exports.fixFemmine(listaClassi[k].nome);
                         console.log(listaClassi[k].proprieta);
                         break;
                     case "stranieri":
@@ -401,14 +401,14 @@ module.exports = {
     },
 
     fixFemmine: function(nomeClasse) {
-        classe = module.exports.findClasseFromString(nomeClasse);
+        var classe = module.exports.findClasseFromString(nomeClasse);
         for (var i = 0; i < listaClassi.length; i++){
             if (listaClassi[i].nome != nomeClasse){
                 if (module.exports.countFemmine(classe.alunni) < module.exports.countFemmine(listaClassi[i].alunni)
                     && module.exports.countFemmine(listaClassi[i].alunni) < settings.fem){
                     var objfem = module.exports.searchAlunno("sesso", "F", classe.alunni);
                     (classe.alunni).splice(objfem, 1);
-                    (listaAlunni[i].alunni).push(objfem);
+                    (listaClassi[i].alunni).push(objfem);
                     console.log("Cambio femmina");
                 }
             }
@@ -420,9 +420,9 @@ module.exports = {
     },
 
     searchAlunno: function(attr, valore, listaAlunniClasse) {
-        for (var i = 0; i < listaAlunniClassi.length; i++){
-            if (listaAlunniClassi[i].attr == valore){
-                return listaAlunniClassi[i];
+        for (var i = 0; i < listaAlunniClasse.length; i++){
+            if (listaAlunniClasse[i].attr == valore){
+                return listaAlunniClasse[i];
             }
         }
         return null;
@@ -478,11 +478,17 @@ module.exports = {
     },
 
     /**
-     * removeNullDaArray rimuove un undefined da un array
+     * removeNullFromArray rimuove un undefined da un array
      * @param array
      */
-    removeNullDaArray: function(array){
+    removeNullFromArray: function(array){
         return array.filter(function(n){ return n != null });
+    },
+
+    removeNullFromListaClassi: function(){
+        for (var i = 0; i < listaClassi; i++){
+            listaClassi[i] = module.exports.removeNullFromArray(listaClassi[i]);
+        }
     }
     //##################################################################################################################
     /**------------------------------------------------FINE UTILITY---------------------------------------------------*/
