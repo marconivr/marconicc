a81e51a0898cc5fa40506fd6bf552e8482097279
francesco taioli <taioli.francesco98@gmail.com>
2017-05-04 12:47:08 +0200

    try to get globals

--- routes/data.js
+++ routes/data.js
@@ -193,6 +193,93 @@ module.exports = function (app, passport, upload) {
 
     });
 
+                                                                                    //TRY
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Dummy users
+    var users = [
+        { id: 0, name: 'tj', email: 'tj@vision-media.ca', role: 'member' }
+        , { id: 1, name: 'ciaran', email: 'ciaranj@gmail.com', role: 'member' }
+        , { id: 2, name: 'aaron', email: 'aaron.heckmann+github@gmail.com', role: 'admin' }
+    ];
+
+    function loadUser(req, res, next) {
+        // You would fetch your user from the db
+        var user = users[req.params.id];
+        if (user) {
+            req.user = user;
+            next();
+        } else {
+            next(new Error('Failed to load user ' + req.params.id));
+        }
+    }
+
+
+
+    function andRestrictToSelf(req, res, next) {
+        // If our authenticated user is the user we are viewing
+        // then everything is fine :)
+        if (req.authenticatedUser.id == req.user.id) {
+            next();
+        } else {
+            // You may want to implement specific exceptions
+            // such as UnauthorizedError or similar so that you
+            // can handle these can be special-cased in an error handler
+            // (view ./examples/pages for this)
+            next(new Error('Unauthorized'));
+        }
+    }
+
+    function andRestrictTo(role) {
+        return function(req, res, next) {
+            res.locals.user = "aa";
+            if (middleware.globalVar.id == role) {
+                next();
+            } else {
+                next(new Error('Unauthorized'));
+            }
+        }
+    }
+
+// Middleware for faux authentication
+// you would of course implement something real,
+// but this illustrates how an authenticated user
+// may interact with middleware
+
+    app.use(function(req, res, next){
+        req.authenticatedUser = users[0];
+        next();
+    });
+
+    app.get('/', function(req, res){
+        res.redirect('/user/0');
+    });
+
+    app.get('/user/:id', loadUser,  andRestrictTo('admin'),  function(req, res){
+        res.send('Viewing user ' + req.user.name);
+    });
+
+    app.get('/user/:id/edit', loadUser, andRestrictToSelf, function(req, res){
+        res.send('Editing user ' + req.user.name);
+    });
+
+    app.delete('/user/:id', loadUser, andRestrictTo('admin'), function(req, res){
+        res.send('Deleted user ' + req.user.name);
+    });
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     app.post('/move-student', middleware.isLoggedIn, function (req, res) {
 
         //update student class
