24c82f8f372902973c6be4e8b304b07d848321a1
marcioz98 <marciozgaming@gmail.com>
2018-03-14 08:51:10 +0100

    Fixed user if exists

--- config/crea_utenti_e_permessi.sql
+++ config/crea_utenti_e_permessi.sql
@@ -1,4 +1,4 @@
-DROP USER 'composizione_classi'@'localhost';
+DROP USER IF EXISTS 'composizione_classi'@'localhost';
 FLUSH PRIVILEGES;
 CREATE USER 'composizione_classi'@'localhost' IDENTIFIED BY '5BFF9B615FBEDCD197BFB9371BB5A7D1';
 GRANT ALL ON composizione_classi.* TO 'composizione_classi'@'localhost';
